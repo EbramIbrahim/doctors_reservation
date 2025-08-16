9a1398e4122e949fefc5349ae29ccd6a98de619c
EbramIbrahim <104721363+EbramIbrahim@users.noreply.github.com>
2025-08-16 16:57:23 +0300

    Add styles for login screen

--- lib/core/theming/text_styles.dart
+++ lib/core/theming/text_styles.dart
@@ -1,5 +1,4 @@
 import 'package:doctors_reservation/core/theming/app_colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -15,10 +14,35 @@ class TextStyles {
       color: AppColors.mainBlue,
       fontWeight: FontWeight.bold
   );
+
+  static TextStyle font24BlueBold = TextStyle(
+      fontSize: 24.sp,
+      color: AppColors.mainBlue,
+      fontWeight: FontWeight.bold
+  );
+
   static TextStyle font13GrayRegular = TextStyle(
       fontSize: 13.sp,
       color: AppColors.gray,
       fontWeight: FontWeight.normal
+  );
+
+  static TextStyle font13BlueRegular = TextStyle(
+      fontSize: 13.sp,
+      color: AppColors.mainBlue,
+      fontWeight: FontWeight.normal
+  );
+
+  static TextStyle font14LightGrayRegular = TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.lightGray,
+      fontWeight: FontWeight.normal
+  );
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.darkBlue,
+      fontWeight: FontWeight.w500
   );
 
   static TextStyle font16WhiteSemiBold = TextStyle(
